84b17666fda3f70f86314ea92d9ee941be436df6
smelc <hgamesdeveloper@gmail.com>
2017-03-13 15:41:52 +0100

    Add Iterables

---
+++ src/main/java/com/hgames/lib/collection/iterable/Iterables.java
@@ -0,0 +1,25 @@
+package com.hgames.lib.collection.iterable;
+
+import java.util.Iterator;
+
+/**
+ * Utility methods about {@link Iterable}.
+ * 
+ * @author smelC
+ */
+public class Iterables {
+
+	/**
+	 * @param it
+	 * @return An iterable backed up by {@code it}.
+	 */
+	public static <T> Iterable<T> ofIterator(final Iterator<T> it) {
+		return new Iterable<T>() {
+			@Override
+			public Iterator<T> iterator() {
+				return it;
+			}
+		};
+	}
+
+}
