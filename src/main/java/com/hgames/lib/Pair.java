7337c0e9ba96107683e6351f1e9bac04fbeb1432
smelc <hgamesdeveloper@gmail.com>
2017-08-17 08:14:21 +0200

    Pair: implement hashCode and equals

--- src/main/java/com/hgames/lib/Pair.java
+++ src/main/java/com/hgames/lib/Pair.java
@@ -44,6 +44,37 @@ public class Pair<T, U> implements Serializable {
 		return snd;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fst == null) ? 0 : fst.hashCode());
+		result = prime * result + ((snd == null) ? 0 : snd.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Pair<?, ?> other = (Pair<?, ?>) obj;
+		if (fst == null) {
+			if (other.fst != null)
+				return false;
+		} else if (!fst.equals(other.fst))
+			return false;
+		if (snd == null) {
+			if (other.snd != null)
+				return false;
+		} else if (!snd.equals(other.snd))
+			return false;
+		return true;
+	}
+
 	/**
 	 * @param t
 	 * @param u
